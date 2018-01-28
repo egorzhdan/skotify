2460e173486a9565b675e09e7f8f18a47d8d3f10
Egor Zhdan <egor.zhdan@gmail.com>
2018-01-28 18:59:45 +0300

    Get credentials from the environment

--- skotify-auth/src/test/com/egorzh/skotify/auth/SecretsHelpers.kt
+++ skotify-auth/src/test/com/egorzh/skotify/auth/SecretsHelpers.kt
@@ -1,9 +1,18 @@
 package com.egorzh.skotify.auth
 
 import java.io.File
+import org.slf4j.LoggerFactory
 
 fun loadSecrets() {
-    val lines = File("../skotify-auth/src/test/resources/secrets/spotify-secrets.txt").readLines()
-    Credentials.clientID = lines[0]
-    Credentials.clientSecret = lines[1]
+    val file = File("../skotify-auth/src/test/resources/secrets/spotify-secrets.txt")
+    if (file.exists()) {
+        LoggerFactory.getLogger("TEST").debug("Secrets file found")
+        val lines = file.readLines()
+        Credentials.clientID = lines[0]
+        Credentials.clientSecret = lines[1]
+    } else {
+        LoggerFactory.getLogger("TEST").debug("Secrets file not found, getting credentials from env")
+        Credentials.clientID = System.getenv("SPOTIFY_CLIENT_ID")
+        Credentials.clientSecret = System.getenv("SPOTIFY_CLIENT_SECRET")
+    }
 }
